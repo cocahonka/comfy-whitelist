bff87de1ea6f5a3477803d2071bc6f855a8044a3
vmlakk <lodyginap@gmail.com>
2023-05-08 17:45:54 +0300

    initialize configs in main plugin class

--- src/main/kotlin/com/cocahonka/comfywhitelist/ComfyWhitelist.kt
+++ src/main/kotlin/com/cocahonka/comfywhitelist/ComfyWhitelist.kt
@@ -1,13 +1,23 @@
 package com.cocahonka.comfywhitelist
 
+import com.cocahonka.comfywhitelist.config.general.GeneralConfig
+import com.cocahonka.comfywhitelist.config.message.MessageConfig
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused")
 class ComfyWhitelist : JavaPlugin() {
+    private lateinit var generalConfig: GeneralConfig
+    private lateinit var messageConfig: MessageConfig
 
     override fun onEnable() {
-        saveDefaultConfig()
         logger.info("hello world!")
+
+        generalConfig = GeneralConfig(this)
+        generalConfig.loadConfig()
+
+        messageConfig = MessageConfig(this, GeneralConfig.locale)
+        messageConfig.loadConfig()
+
     }
 
     override fun onDisable() {}
