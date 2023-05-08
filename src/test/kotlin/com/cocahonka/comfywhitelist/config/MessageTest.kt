6f442a21643b2cd54543ff25f99ac593f18ab32c
vmlakk <lodyginap@gmail.com>
2023-05-08 13:32:52 +0300

    cover getMessageWithDefault with test

---
+++ src/test/kotlin/com/cocahonka/comfywhitelist/config/MessageTest.kt
@@ -0,0 +1,35 @@
+package com.cocahonka.comfywhitelist.config
+
+import com.cocahonka.comfywhitelist.config.Message.Companion.getMessageWithDefault
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.configuration.file.YamlConfiguration
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class MessageTest {
+    @Test
+    fun `getMessageWithDefault returns default message when configuration is empty`() {
+        val locale = Locale.RU
+        val defaultMessage = Message.WhitelistDisabled.getDefault(locale)
+
+        val emptyFileConfiguration: FileConfiguration = YamlConfiguration()
+
+        val messageWithDefault = emptyFileConfiguration.getMessageWithDefault(Message.WhitelistDisabled, locale)
+
+        assertEquals(defaultMessage, messageWithDefault)
+    }
+
+    @Test
+    fun `getMessageWithDefault returns configured message when configuration is not empty`() {
+        val locale = Locale.RU
+        val configuredMessage = "Configured message"
+
+        val configFileConfiguration: FileConfiguration = YamlConfiguration()
+        configFileConfiguration.set(Message.WhitelistDisabled.key, configuredMessage)
+
+        val messageWithDefault = configFileConfiguration.getMessageWithDefault(Message.WhitelistDisabled, locale)
+
+        assertEquals(configuredMessage, messageWithDefault)
+    }
+
+}
