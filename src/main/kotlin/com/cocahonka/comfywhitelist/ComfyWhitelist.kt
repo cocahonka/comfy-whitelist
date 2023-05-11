00a46c7894a01628694275435f5549d1674d745e
cocahonka <cocahonka@gmail.com>
2023-05-11 11:46:09 +0300

    add constructor for unit tests

--- src/main/kotlin/com/cocahonka/comfywhitelist/ComfyWhitelist.kt
+++ src/main/kotlin/com/cocahonka/comfywhitelist/ComfyWhitelist.kt
@@ -1,25 +1,62 @@
+@file:Suppress("removal")
+
 package com.cocahonka.comfywhitelist
 
 import com.cocahonka.comfywhitelist.config.general.GeneralConfig
 import com.cocahonka.comfywhitelist.config.message.MessageConfig
+import com.cocahonka.comfywhitelist.storage.YamlStorage
+import org.bukkit.plugin.PluginDescriptionFile
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.java.JavaPluginLoader
+import java.io.File
 
 @Suppress("unused")
-class ComfyWhitelist : JavaPlugin() {
+class ComfyWhitelist : JavaPlugin {
+    constructor() : super() {
+        isUnitTest = false
+    }
+
+    /**
+     * Constructor ONLY for unit tests
+     */
+    @Suppress("removal", "DEPRECATION")
+    constructor(
+        loader: JavaPluginLoader,
+        description: PluginDescriptionFile,
+        dataFolder: File,
+        file: File
+    ) : super(loader, description, dataFolder, file) {
+        isUnitTest = true
+    }
+
+    private val isUnitTest: Boolean
     private lateinit var generalConfig: GeneralConfig
     private lateinit var messageConfig: MessageConfig
+    private lateinit var storage: YamlStorage
 
     override fun onEnable() {
-        logger.info("hello world!")
+        if(isUnitTest) onUnitTest()
+        else onPluginEnable()
+    }
 
+    override fun onDisable() {}
+
+    private fun onUnitTest() {}
+
+    private fun onPluginEnable() {
+        loadConfigs()
+        loadStorage()
+    }
+
+    private fun loadConfigs() {
         generalConfig = GeneralConfig(this)
         generalConfig.loadConfig()
 
         messageConfig = MessageConfig(this, GeneralConfig.locale)
         messageConfig.loadConfig()
-
     }
 
-    override fun onDisable() {}
-
+    private fun loadStorage() {
+        storage = YamlStorage(dataFolder)
+    }
 }
