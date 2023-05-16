c45ef955b3e1cf369267323a943d5d306a71214a
cocahonka <cocahonka@gmail.com>
2023-05-16 19:41:28 +0300

    refactoring message

--- src/main/kotlin/com/cocahonka/comfywhitelist/config/message/Message.kt
+++ src/main/kotlin/com/cocahonka/comfywhitelist/config/message/Message.kt
@@ -1,6 +1,8 @@
 package com.cocahonka.comfywhitelist.config.message
 
 import com.cocahonka.comfywhitelist.config.base.Locale
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.configuration.file.FileConfiguration
 
 /**
@@ -14,7 +16,9 @@ sealed class Message(val key: String) {
      * @param locale The locale to get the default message for.
      * @return The default message for the specified locale.
      */
-    abstract fun getDefault(locale: Locale): String
+    abstract fun getDefault(locale: Locale): Component
+
+    abstract fun applyStyles(rawMessage: String): Component
 
     companion object {
 
@@ -28,121 +32,269 @@ sealed class Message(val key: String) {
          */
         fun <M : Message> FileConfiguration.getMessageWithDefault(
             message: M,
-            locale: Locale
-        ): String {
-            return this.getString(message.key) ?: message.getDefault(locale)
+            locale: Locale,
+        ): Component {
+            val rawMessageFromConfig = this.getString(message.key)
+
+            return if(rawMessageFromConfig == null) {
+                message.getDefault(locale)
+            } else {
+                message.applyStyles(rawMessageFromConfig)
+            }
         }
+
+        val miniMessage = MiniMessage.miniMessage()
     }
 
-    object NotWhitelisted : Message("not-whitelisted") {
-        override fun getDefault(locale: Locale): String = when (locale) {
-            Locale.RU -> "Вы не в вайтлисте."
-            Locale.EN -> "You are not whitelisted."
+    object General {
+        object NoPermission : Message("no-permission") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
         }
+
+        object InvalidUsage : Message("invalid-usage") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
+        object UnknownSubcommand : Message("unknown-subcommand") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
+        object InvalidPlayerName : Message("invalid-player-name") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
+        object PluginReloaded : Message("plugin-reloaded") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
     }
 
-    object PlayerAdded : Message("player-added") {
-        override fun getDefault(locale: Locale): String = when (locale) {
-            Locale.RU -> "Игрок %s добавлен в вайтлист."
-            Locale.EN -> "Player %s has been added to the whitelist."
+    object WhitelistStatus {
+        object WhitelistEnabled : Message("whitelist-enabled") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
         }
+
+        object WhitelistDisabled : Message("whitelist-disabled") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
+        object WhitelistAlreadyEnabled : Message("whitelist-already-enabled") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
+        object WhitelistAlreadyDisabled : Message("whitelist-already-disabled") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
     }
 
-    object PlayerRemoved : Message("player-removed") {
-        override fun getDefault(locale: Locale): String = when (locale) {
-            Locale.RU -> "Игрок %s удален из вайтлиста."
-            Locale.EN -> "Player %s has been removed from the whitelist."
+    object PlayerManagement {
+        object NotWhitelisted : Message("not-whitelisted") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+
         }
+
+        object PlayerAdded : Message("player-added") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
+        object PlayerRemoved : Message("player-removed") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
+        object NonExistentPlayerName : Message("non-existent-player-name") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
+        }
+
     }
 
-    object WhitelistCleared : Message("whitelist-cleared") {
-        override fun getDefault(locale: Locale): String = when (locale) {
-            Locale.RU -> "Все игроки удалены из вайтлиста."
-            Locale.EN -> "All players have been removed from the whitelist."
-        }
-    }
+    object WhitelistDisplay {
+        object WhitelistedPlayersList : Message("whitelisted-players-list") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
 
-    object WhitelistEnabled : Message("whitelist-enabled") {
-        override fun getDefault(locale: Locale): String = when (locale) {
-            Locale.RU -> "ComfyWhitelist включен."
-            Locale.EN -> "ComfyWhitelist enabled."
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
         }
-    }
 
-    object WhitelistDisabled : Message("whitelist-disabled") {
-        override fun getDefault(locale: Locale): String = when (locale) {
-            Locale.RU -> "ComfyWhitelist выключен."
-            Locale.EN -> "ComfyWhitelist disabled."
+        object EmptyWhitelistedPlayersList : Message("empty-whitelisted-players-list") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
+
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
         }
-    }
 
-    object PluginReloaded : Message("plugin-reloaded") {
-        override fun getDefault(locale: Locale): String = when (locale) {
-            Locale.RU -> "ComfyWhitelist успешно перезагружен."
-            Locale.EN -> "ComfyWhitelist has been successfully reloaded."
-        }
-    }
+        object WhitelistCleared : Message("whitelist-cleared") {
+            override fun getDefault(locale: Locale): Component = applyStyles(
+                when (locale) {
+                    Locale.RU -> "Вы не в вайтлисте."
+                    Locale.EN -> "You are not whitelisted."
+                }
+            )
 
-    object NoPermission : Message("no-permission") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "У вас недостаточно полномочий для использования этой команды."
-            Locale.EN -> "You do not have permission to use this command."
-        }
-    }
-
-    object InvalidUsage : Message("invalid-usage") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "Недопустимое использование команды (правильное использование: %s)"
-            Locale.EN -> "Invalid command usage. (correct use: %s)"
-        }
-    }
-
-    object WhitelistedPlayersList : Message("whitelisted-players-list") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "Игроки в вайтлисте: %s"
-            Locale.EN -> "Whitelisted players: %s"
-        }
-    }
-
-    object EmptyWhitelistedPlayersList : Message("empty-whitelisted-players-list") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "В вайтлисте нет игроков."
-            Locale.EN -> "Whitelist is empty."
-        }
-    }
-
-    object UnknownSubcommand: Message("unknown-subcommand") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "Неизвестная подкоманда. Введите /comfywl help для отображения доступных подкоманд."
-            Locale.EN -> "Unknown subcommand. Type /comfywl help for a list of commands."
-        }
-    }
-
-    object InvalidPlayerName: Message("invalid-player-name") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "Некорректный формат имени игрока."
-            Locale.EN -> "Invalid player name."
-        }
-    }
-
-    object NonExistentPlayerName : Message("non-existent-player-name") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "Игрока с именем %s нет в вайтлисте."
-            Locale.EN -> "There is no player named %s in the whitelist."
-        }
-    }
-
-    object WhitelistAlreadyEnabled : Message("whitelist-already-enabled") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "ComfyWhitelist уже включен."
-            Locale.EN -> "ComfyWhitelist already enabled."
-        }
-    }
-
-    object WhitelistAlreadyDisabled : Message("whitelist-already-disabled") {
-        override fun getDefault(locale: Locale): String = when(locale) {
-            Locale.RU -> "ComfyWhitelist уже выключен."
-            Locale.EN -> "ComfyWhitelist already disabled."
+            override fun applyStyles(rawMessage: String): Component =
+                miniMessage.deserialize(
+                    rawMessage,
+                    MessageTagResolvers.warning
+                )
         }
     }
 
--- src/main/kotlin/com/cocahonka/comfywhitelist/config/message/MessageConfig.kt
+++ src/main/kotlin/com/cocahonka/comfywhitelist/config/message/MessageConfig.kt
@@ -3,6 +3,7 @@ package com.cocahonka.comfywhitelist.config.message
 import com.cocahonka.comfywhitelist.config.base.ConfigManager
 import com.cocahonka.comfywhitelist.config.base.Locale
 import com.cocahonka.comfywhitelist.config.message.Message.Companion.getMessageWithDefault
+import net.kyori.adventure.text.Component
 import org.bukkit.plugin.Plugin
 import java.io.File
 
@@ -15,22 +16,30 @@ import java.io.File
  */
 class MessageConfig(private val plugin: Plugin, private val locale: Locale) : ConfigManager() {
     companion object {
-        lateinit var notWhitelisted: String private set
-        lateinit var playerAdded: String private set
-        lateinit var playerRemoved: String private set
-        lateinit var whitelistCleared: String private set
-        lateinit var whitelistEnabled: String private set
-        lateinit var whitelistDisabled: String private set
-        lateinit var pluginReloaded: String private set
-        lateinit var noPermission: String private set
-        lateinit var invalidUsage: String private set
-        lateinit var whitelistedPlayersList: String private set
-        lateinit var emptyWhitelistedPlayersList: String private set
-        lateinit var unknownSubcommand: String private set
-        lateinit var invalidPlayerName: String private set
-        lateinit var nonExistentPlayerName : String private set
-        lateinit var whitelistAlreadyEnabled: String private set
-        lateinit var whitelistAlreadyDisabled: String private set
+        // General messages
+        lateinit var noPermission: Component internal set
+        lateinit var invalidUsage: Component private set
+        lateinit var unknownSubcommand: Component private set
+        lateinit var invalidPlayerName: Component private set
+        lateinit var pluginReloaded: Component private set
+
+        // Whitelist status messages
+        lateinit var whitelistEnabled: Component private set
+        lateinit var whitelistDisabled: Component private set
+        lateinit var whitelistAlreadyEnabled: Component private set
+        lateinit var whitelistAlreadyDisabled: Component private set
+
+        // Player management messages
+        lateinit var notWhitelisted: Component private set
+        lateinit var playerAdded: Component private set
+        lateinit var playerRemoved: Component private set
+        lateinit var nonExistentPlayerName: Component private set
+
+        // Whitelist display messages
+        lateinit var whitelistedPlayersList: Component private set
+        lateinit var emptyWhitelistedPlayersList: Component private set
+        lateinit var whitelistCleared: Component private set
+
     }
 
     override fun createConfig() {
@@ -50,22 +59,29 @@ class MessageConfig(private val plugin: Plugin, private val locale: Locale) : Co
     }
 
     override fun updateProperties() {
-        notWhitelisted = config.getMessageWithDefault(Message.NotWhitelisted, locale)
-        playerAdded = config.getMessageWithDefault(Message.PlayerAdded, locale)
-        playerRemoved = config.getMessageWithDefault(Message.PlayerRemoved, locale)
-        whitelistCleared = config.getMessageWithDefault(Message.WhitelistCleared, locale)
-        whitelistEnabled = config.getMessageWithDefault(Message.WhitelistEnabled, locale)
-        whitelistDisabled = config.getMessageWithDefault(Message.WhitelistDisabled, locale)
-        pluginReloaded = config.getMessageWithDefault(Message.PluginReloaded, locale)
-        noPermission = config.getMessageWithDefault(Message.NoPermission, locale)
-        invalidUsage = config.getMessageWithDefault(Message.InvalidUsage, locale)
-        whitelistedPlayersList = config.getMessageWithDefault(Message.WhitelistedPlayersList, locale)
-        emptyWhitelistedPlayersList = config.getMessageWithDefault(Message.EmptyWhitelistedPlayersList, locale)
-        unknownSubcommand = config.getMessageWithDefault(Message.UnknownSubcommand, locale)
-        invalidPlayerName = config.getMessageWithDefault(Message.InvalidPlayerName, locale)
-        nonExistentPlayerName = config.getMessageWithDefault(Message.NonExistentPlayerName, locale)
-        whitelistAlreadyEnabled = config.getMessageWithDefault(Message.WhitelistAlreadyEnabled, locale)
-        whitelistAlreadyDisabled = config.getMessageWithDefault(Message.WhitelistAlreadyDisabled, locale)
+        // General messages
+        noPermission = config.getMessageWithDefault(Message.General.NoPermission, locale)
+        invalidUsage = config.getMessageWithDefault(Message.General.InvalidUsage, locale)
+        unknownSubcommand = config.getMessageWithDefault(Message.General.UnknownSubcommand, locale)
+        invalidPlayerName = config.getMessageWithDefault(Message.General.InvalidPlayerName, locale)
+        pluginReloaded = config.getMessageWithDefault(Message.General.PluginReloaded, locale)
+
+        // Whitelist status messages
+        whitelistEnabled = config.getMessageWithDefault(Message.WhitelistStatus.WhitelistEnabled, locale)
+        whitelistDisabled = config.getMessageWithDefault(Message.WhitelistStatus.WhitelistDisabled, locale)
+        whitelistAlreadyEnabled = config.getMessageWithDefault(Message.WhitelistStatus.WhitelistAlreadyEnabled, locale)
+        whitelistAlreadyDisabled = config.getMessageWithDefault(Message.WhitelistStatus.WhitelistAlreadyDisabled, locale)
+
+        // Player management messages
+        notWhitelisted = config.getMessageWithDefault(Message.PlayerManagement.NotWhitelisted, locale)
+        playerAdded = config.getMessageWithDefault(Message.PlayerManagement.PlayerAdded, locale)
+        playerRemoved = config.getMessageWithDefault(Message.PlayerManagement.PlayerRemoved, locale)
+        nonExistentPlayerName = config.getMessageWithDefault(Message.PlayerManagement.NonExistentPlayerName, locale)
+
+        // Whitelist display messages
+        whitelistedPlayersList = config.getMessageWithDefault(Message.WhitelistDisplay.WhitelistedPlayersList, locale)
+        emptyWhitelistedPlayersList = config.getMessageWithDefault(Message.WhitelistDisplay.EmptyWhitelistedPlayersList, locale)
+        whitelistCleared = config.getMessageWithDefault(Message.WhitelistDisplay.WhitelistCleared, locale)
     }
 
 }
