0932963f3d188fedf6044eeb4628c49f915bdc32
cocahonka <cocahonka@gmail.com>
2023-05-18 00:56:11 +0300

    add standalone and lightweight build task

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.apache.tools.ant.filters.ReplaceTokens
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "1.8.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -29,27 +29,55 @@ dependencies {
     testImplementation("com.github.seeseemelk:MockBukkit-v$mockBukkitVersion")
 }
 
+configurations {
+    create("withoutKotlinStdlib") {
+        extendsFrom(configurations.runtimeClasspath.get())
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+    }
+}
+
 tasks {
     test {
         useJUnitPlatform()
     }
+
     processResources {
         filter(FixCrLfFilter::class)
         filter(ReplaceTokens::class, "tokens" to mapOf("version" to project.version))
         filteringCharset = "UTF-8"
     }
+
     jar {
         enabled = false
     }
 
     val shadowJar by existing(ShadowJar::class) {
-        archiveFileName.set("ComfyWhitelist-$version.jar")
-        destinationDirectory.set(layout.buildDirectory.dir("../../paper_server/plugins"))
+        archiveFileName.set("ComfyWhitelist-$version-standalone.jar")
+        destinationDirectory.set(layout.buildDirectory.dir("libs"))
     }
 
-    build {
+    val shadowJarLightweight by creating(ShadowJar::class) {
+        archiveFileName.set("ComfyWhitelist-$version-lightweight.jar")
+        destinationDirectory.set(layout.buildDirectory.dir("libs"))
+        configurations = listOf(project.configurations["withoutKotlinStdlib"])
+        from(sourceSets.main.get().output)
+
+        exclude("META-INF/*.kotlin_module")
+        exclude("META-INF/*.DSA")
+        exclude("META-INF/*.RSA")
+        exclude("META-INF/*.SF")
+    }
+
+    register("standalone") {
+        group = "build"
         dependsOn(shadowJar)
     }
+
+    register("lightweight") {
+        group = "build"
+        dependsOn(shadowJarLightweight)
+    }
+
     withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = "17"
